fe98135c5084fbc422b3f931e14dc8722b72b033
Nailiya <66hamsa@gmail.com>
2023-01-30 14:04:28 +0700

    init

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,90 @@
+package data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.Filter;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.*;
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.*;
+import lombok.Value;
+
+import javax.xml.bind.annotation.XmlEnumValue;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.security.KeyStore;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(Registration.RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+
+    }
+
+    public static String getRandomLogin() {
+        String login = faker.name().username();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            var user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            return user;
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+
+        @Value
+        public static class RegistrationDto {
+
+            String login;
+            String password;
+            String status;
+        }
+
+    }
+}
+
